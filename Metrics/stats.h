db4d1dd63f39ebb8a6ff0e2cb915e384dc48382a
Evan <115374841+evanwporter@users.noreply.github.com>
2023-09-24 12:02:12 -0700

    Merged.

--- Metrics/stats.h
+++ Metrics/stats.h
@@ -26,10 +26,44 @@ MatrixXd covariance(MatrixXd M) {
     // Returns covariance matrix
     MatrixXd centered = RETURNS.rowwise() - RETURNS.colwise().mean();
     MatrixXd cov = (centered.adjoint() * centered) / double(RETURNS.rows() - 1);
-
-    auto Dinv = cov.inverse();
-
-    // std::cout << cov;
-
-    return Dinv * cov * Dinv;
 }
+
+double cov(VectorXd x, VectorXd y) {
+    // Returns scalar quantity
+    VectorXd xM = (x.array() - x.mean());
+    VectorXd yM = (y.array() - y.mean());
+
+<<<<<<< HEAD
+    VectorXd MM = xM.array() * yM.array();
+
+    return MM.sum() / x.size();
+}
+
+MatrixXd correlation(MatrixXd COV){
+    // Returns correlation matrix
+    auto Dinv = COV.inverse();
+    return Dinv * COV * Dinv;
+}
+
+void cumulative_product(Eigen::VectorXd vec, Eigen::VectorXd& make_vec, float offset = 0) {
+
+    make_vec = Eigen::MatrixXd(vec.size(), 1);
+
+    make_vec(0) = vec(0);
+    vec.array() += offset;
+
+    for (int j = 1; j < vec.size(); j++) {
+        make_vec(j) = (vec(j) * make_vec(j - 1));
+    };
+
+}
+=======
+    // std::cout << cov; 
+    // https://www.geeksforgeeks.org/convert-covariance-matrix-to-correlation-matrix-using-python/amp/
+    return Dinv * cov * Dinv;
+
+
+
+    
+}
+>>>>>>> ab20e137e851b393afb4a567d191e4b45ed32fe4
